4b29f17cb8da38dc82e8ecc727a84a4235ad7a1e
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2024-07-10 11:06:34 +0100

    feat: models added

---
+++ src/models/configuration.model.mjs
@@ -0,0 +1,13 @@
+// Configuration model
+
+export const SCRIPT_MODEL = {
+    name: undefined,
+    command: undefined,
+    result: undefined,
+};
+
+export const GATE_KEEPER_CONFIG_MODEL = {
+    port: undefined,
+    host: undefined,
+    scripts: [SCRIPT_MODEL],
+};
