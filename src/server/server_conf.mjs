ed2837c02ac6fbbd3521a76d16089fa88fc29d6c
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2024-07-31 11:34:13 +0100

    feat: updated cancommit and now works

--- src/server/server_conf.mjs
+++ src/server/server_conf.mjs
@@ -10,6 +10,9 @@ import { __dirname } from '../libs/app_utils.mjs';
 // Certificate creation
 const pems = selfsigned.generate(null, { days: 365 });
 
+// Import state
+import { STATE } from '../libs/state.mjs';
+
 // Application creation and setup
 
 export const express_app = express();
@@ -20,8 +23,12 @@ export const express_ws_port = process.env.GATE_KEEPER_WS_PORT || 9001;
 export const isHTTPS = (process.env.GATE_KEEPER_HTTPS === 'false' ? false : true) || true
 
 // Static assets
-// express_app.red
 express_app.use(express.static(`${__dirname}/../../public`));
+
+// Can commit endpoint
+express_app.get('/cancommit', (req, res) => {
+    res.json({ cancommit: STATE.canCommit });
+});
 
 // Create server
 let express_server = undefined;
